32873bcaf1b382d50deb59f2c0800148c64b4338
drmzfatih <Durmazfa@gmail.com>
2022-01-03 19:45:10 -0500

    125

--- .history/userdata_20220103194402.sh
+++ .history/userdata_20220103194402.sh
@@ -7,3 +7,4 @@ cd /var/www/html
 wget https://aws-tc-largeobjects.s3-us-west-2.amazonaws.com/CUR-TF-200-ACACAD/studentdownload/lab-app.tgz
 tar xvfz lab-app.tgz
 chown apache:root /var/www/html/rds.conf.php
+fi
