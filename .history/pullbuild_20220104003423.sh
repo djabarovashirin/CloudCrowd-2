0d3a8309218041b4793ef390e88d4b814a356f1a
drmzfatih <Durmazfa@gmail.com>
2022-01-04 00:39:00 -0500

    test

---
+++ .history/pullbuild_20220104003423.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+aws s3 cp s3://s3ccbucket/12312/userdata.sh /tmp/
+aws s3 cp s3://s3ccbucket/12312/appspec.zip /tmp/
---
+++ .history/pullbuild_20220104003853.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+aws s3 cp s3://s3ccbucket/12312/userdata.sh /tmp/
