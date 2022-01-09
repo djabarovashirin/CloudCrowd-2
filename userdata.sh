82e3ccaf9508c94267518bfd8bc0eb8bc1108d4a
drmzfatih <Durmazfa@gmail.com>
2022-01-09 00:57:44 -0500

    123

--- userdata.sh
+++ userdata.sh
@@ -4,7 +4,7 @@ if [ ! -f /var/www/html/lab-app.tgz ]; then
 cd /var/www/html
 wget http://download1638.mediafire.com/8jfp6su6v2eg/6e0dz7tswwvfc4i/lab-app.zip
 unzip lab-app.zip
-cd /var/www/lab-app.zip
+cd /var/www/html/lab-app.zip
 mv * /var/www/html/
 cd /var/www/html/
 chown apache:root /var/www/html/rds.conf.php
