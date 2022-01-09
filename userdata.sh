f8e2e45e1b2ade82b5e5f34a8f7a4d6419096c1a
drmzfatih <Durmazfa@gmail.com>
2022-01-09 00:53:11 -0500

    123

--- userdata.sh
+++ userdata.sh
@@ -3,6 +3,6 @@ yum -y install httpd php mysql php-mysql
 if [ ! -f /var/www/html/lab-app.tgz ]; then
 cd /var/www/html
 wget http://download1638.mediafire.com/8jfp6su6v2eg/6e0dz7tswwvfc4i/lab-app.zip
-unzip lab-app.tgz
+unzip lab-app.zip
 chown apache:root /var/www/html/rds.conf.php
 fi
