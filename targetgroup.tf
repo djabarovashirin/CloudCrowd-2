94b585bf6c5f4ca7b19c0bd9d0a093d11275ee9a
drmzfatih <Durmazfa@gmail.com>
2022-01-10 18:50:30 -0500

    test

--- targetgroup.tf
+++ targetgroup.tf
@@ -5,8 +5,11 @@ resource "aws_lb_target_group" "test" {
   vpc_id      = aws_vpc.test.id
   target_type = "instance"
   slow_start= 120
-  inteval = 120
+
+  health_check { 
+  interval = 120
   healthy_threshold = 10
   timeout = 119
+  }
 }
 
