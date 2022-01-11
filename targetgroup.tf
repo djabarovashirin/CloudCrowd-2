3b14cea9e1e289be362592fb9fb8c1f89011ac08
drmzfatih <Durmazfa@gmail.com>
2022-01-10 19:23:01 -0500

    test

--- targetgroup.tf
+++ targetgroup.tf
@@ -4,5 +4,12 @@ resource "aws_lb_target_group" "test" {
   protocol    = "HTTP"
   vpc_id      = aws_vpc.test.id
   target_type = "instance"
+ health_check {
+    protocol            = "HTTP"
+    path                = "/"
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 5
+    interval            = 10
+  }
 }
-
